344502a2bc05ff3b81898d280ede9c5970dd84ee
smajumdar13 <77767611+smajumdar13@users.noreply.github.com>
2021-03-29 15:42:26 -0500

    class3.29

--- Lab5/scripts/app.js
+++ Lab5/scripts/app.js
@@ -1,9 +1,8 @@
 const nums =[1,9,3,22,5];
-
+// the biggest number in the array
 function max(n){
     let temp=0;
     for(let i=0; i<n.length-1; i++){
-        
         if(n[i]>n[i+1]){
             temp=n[i];
         } else {
@@ -14,4 +13,64 @@ function max(n){
 }
 console.log(max(nums));
 document.write(max(nums));
+document.write("<br>");
+
+// biggest number among three numbers
+const maxNumOfThree = [1,2,3];
+function maxOfThree(a,b,c) {
+    let temp=0;
+    if(a>b & a>c) temp = a;
+    else if(b>c & b>a) temp = b;
+    else temp=c;
+    return temp;
+}
+console.log(maxOfThree(42,61,19));
+document.write(maxOfThree(42,61,19));
+document.write("<br>");
+
+// is a vowel or is not
+function isVowel(n) {
+    if(n==="a" || n==="e" || n==="i" || n==="o" || n==="u" ) return true;
+    return false;
+}
+console.log(isVowel("c"));
+document.write(isVowel("c"));
+document.write("<br>");
+
+// sum of all numbers
+const numbers = [1,2,3,4];
+function sum(array){
+    let temp=0;
+    for(let i=0; i<array.length; i++){
+        temp += numbers[i];
+    }
+    return temp;
+}
+console.log(sum(numbers));
+document.write(sum(numbers));
+document.write("<br>");
+
+// product of all numbers
+function multiply(array){
+    let temp=1;
+    for(let i=0; i<array.length; i++){
+        temp *= numbers[i];
+    }
+    return temp;
+}
+console.log(multiply(numbers));
+document.write(multiply(numbers));
+document.write("<br>");
+
+const string = "Hello, world!";
+function reverse(str){
+    let strTemp="";
+    for(let i=0; i<str.length; i++){
+        strTemp += str[str.length-1-i];
+    }
+    return strTemp;
+}
+console.log(reverse(string));
+document.write(reverse(string));
+document.write("<br>");
 
--- Lesson5InClass copy/scripts/app.js
+++ Lesson5InClass copy/scripts/app.js
@@ -1,7 +1,10 @@
 /**
  * app-lesson6.js
+ * addEventListener, map, filter, find, findIndex, reduce, for-loops(for, for in, for of, forEach),
+ *
+ * map, filter, find, and reduce are considered "Pure" functions, and have no side-effects
  */
-
+// window.onload = function() {
 console.log(window);
 console.log(window.location);
 console.log(window.location.href);
@@ -14,7 +17,50 @@ const btnOK = document.getElementById("btnOK");
 //     btnOK.innerText = "Clicked";
 // }
 
-btnOK.addEventListener("click", function(){
-    const btnOK = document.getElementById("btnOK");
-    btnOK.innerText = "Clicked 2";
-})
+btnOK.addEventListener("click", function () {
+  const btnOK = document.getElementById("btnOK");
+  btnOK.innerText = "Clicked 2";
+});
+
+const arr = [1, 2, 3, 4, 5];
+const newArr = arr.map((s) => s * s);
+console.log(newArr);
+
+const a = ["Hydrogen", "Helium", "Lithium", "Beryllium", "Boron"];
+const a2 = a.map(function (f) {
+  return f.length;
+});
+console.log("a2: " + a2);
+
+const a3 = a.map((f) => f.length);
+console.log("a3: " + a3);
+
+console.log("a4: " + a.map((a) => a.length));
+
+const b1 = a.filter((s) => s.length > 7);
+console.log("b1: " + b1);
+const b2 = a.find((s) => s.length < 7);
+console.log("b2: " + b2);
+const b3 = a.findIndex((s) => s.length > 8);
+console.log("b3: " + b3);
+
+// const initialValue = 0;
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+console.log(arr.reduce(reducer));
+
+a.forEach((e) => console.log("element: " + e));
+
+const sumOfSquares = (arr) =>
+  arr.map((e) => e * e).reduce((acc, sq) => acc + sq);
+console.log(sumOfSquares(arr));
+
+const sumOfSquaresOfEvensOnly = (arr) =>
+  arr
+    .filter((n) => n % 2 === 0)
+    .map((e) => e * e)
+    .reduce((acc, sq) => acc + sq);
+console.log(sumOfSquaresOfEvensOnly(arr));
+
+//
+
+// };
